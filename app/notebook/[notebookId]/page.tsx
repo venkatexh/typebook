5e32d40cd5515917912438b3eb56145207aa88f1
Venkatesh <venkateshchakrabarty@gmail.com>
2026-03-22 22:46:47 +0530

    add: supabase update: notebook route

--- app/notebook/[notebookId]/page.tsx
+++ app/notebook/[notebookId]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CodeCell from "@/components/code/CodeCell";
 import { Cell } from "@/types/code/cell";
 import MarkdownCell from "@/components/code/MarkdownCell";
+import { supabase } from "@/lib/supabase";
+import { useParams } from "next/navigation";
 
 export default function CodePage() {
+  const params = useParams();
   const [cells, setCells] = useState<Cell[]>([]);
 
   const addCodeCell = () => {
@@ -41,8 +44,59 @@ export default function CodePage() {
     return <MarkdownCell cell={cell} />;
   }
 
+  const saveNotebook = async (cells: Cell[]) => {
+    const { data, error } = await supabase
+      .from("notebooks")
+      .update([
+        {
+          title: "Untitled",
+          cells,
+        },
+      ])
+      .eq("id", params.notebookId);
+
+    if (error) {
+      console.log(error);
+    }
+
+    return data;
+  };
+
+  const loadNotebook = async (id: string) => {
+    const { data, error } = await supabase
+      .from("notebooks")
+      .select("*")
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.error(error);
+      return null;
+    }
+
+    return data;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const notebook = await loadNotebook(params.notebookId as string);
+      if (notebook) {
+        setCells(notebook.cells);
+      }
+    };
+
+    fetchData();
+  }, [params.notebookId]);
+
   return (
     <div className='w-full flex flex-col p-4'>
+      <div>
+        <button
+          onClick={() => saveNotebook(cells)}
+          className='px-4 py-1 bg-cyan-900'>
+          Save
+        </button>
+      </div>
       <div className='flex justify-center gap-4'>
         <button onClick={addCodeCell} className='px-4 py-1 border rounded-3xl'>
           + Code
@@ -56,7 +110,7 @@ export default function CodePage() {
       <div>
         {cells.map((cell) => (
           <div key={cell.id}>
-            <CellRenderer key={cell.id} cell={cell} />
+            <CellRenderer cell={cell} />
             <div className='flex justify-center gap-4'>
               <button
                 onClick={addCodeCell}
