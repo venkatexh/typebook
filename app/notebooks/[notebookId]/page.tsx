fe20a1f4258e6425eb9895d69f90a331f8b3c304
Venkatesh <venkateshchakrabarty@gmail.com>
2026-03-23 00:50:41 +0530

    update: handle code update

--- app/notebooks/[notebookId]/page.tsx
+++ app/notebooks/[notebookId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import CodeCell from "@/components/code/CodeCell";
 import { Cell } from "@/types/code/cell";
 import MarkdownCell from "@/components/code/MarkdownCell";
@@ -13,16 +13,12 @@ export default function CodePage() {
 
   const addCodeCell = () => {
     setCells([
-      ...cells,
       {
         id: crypto.randomUUID(),
         type: "code",
-        content: `
-    export default function App() {
-  return <h1>JSX is working 🎉</h1>;
-}
-  `,
+        content: `const App = () => <h1>Hi there.</h1>`,
       },
+      ...cells,
     ]);
   };
 
@@ -37,12 +33,16 @@ export default function CodePage() {
     ]);
   };
 
-  function CellRenderer({ cell }: { cell: Cell }) {
-    if (cell.type === "code") {
-      return <CodeCell cell={cell} />;
-    }
-    return <MarkdownCell cell={cell} />;
-  }
+  const handleChange = useCallback((content: string, id: string) => {
+    setCells((prevCells) =>
+      prevCells.map((cell) => {
+        if (cell.id === id) {
+          return { ...cell, content };
+        }
+        return cell;
+      }),
+    );
+  }, []);
 
   const saveNotebook = async (cells: Cell[]) => {
     const { data, error } = await supabase
@@ -108,23 +108,17 @@ export default function CodePage() {
         </button>
       </div>
       <div>
-        {cells.map((cell) => (
-          <div key={cell.id}>
-            <CellRenderer cell={cell} />
-            <div className='flex justify-center gap-4'>
-              <button
-                onClick={addCodeCell}
-                className='px-4 py-1 border rounded-3xl'>
-                + Code
-              </button>
-              <button
-                onClick={addMarkdownCell}
-                className='px-4 py-1 border rounded-3xl'>
-                + Text
-              </button>
-            </div>
-          </div>
-        ))}
+        {cells.map((cell) =>
+          cell.type === "code" ? (
+            <CodeCell
+              key={cell.id}
+              content={cell.content}
+              onChange={(v) => handleChange(v || "", cell.id)}
+            />
+          ) : (
+            <MarkdownCell key={cell.id} cell={cell} />
+          ),
+        )}
       </div>
     </div>
   );
--- components/code/CodeCell.tsx
+++ components/code/CodeCell.tsx
@@ -1,23 +1,34 @@
 import { Editor } from "@monaco-editor/react";
-import { useState } from "react";
 import Preview from "./Preview";
-import { Cell } from "@/types/code/cell";
+import React from "react";
 
-function CodeCell({ cell }: { cell: Cell }) {
-  const [code, setCode] = useState(cell.content);
-
+const CodeCell = React.memo(function CodeCell({
+  content,
+  onChange,
+}: CodeCellProps) {
   return (
-    <div className='flex rounded'>
-      <Editor
-        height='300px'
-        defaultLanguage='javascript'
-        value={code}
-        onChange={(v) => setCode(v || "")}
-        theme='vs-dark'
-      />
-      <Preview code={code} />
+    <div>
+      <div className='flex rounded'>
+        <Editor
+          height='300px'
+          defaultLanguage='javascript'
+          defaultValue={content}
+          onChange={(v) => onChange(v)}
+          theme='vs-dark'
+        />
+        <Preview code={content} />
+      </div>
+      <div className='flex justify-center gap-4'>
+        <button className='px-4 py-1 border rounded-3xl'>+ Code</button>
+        <button className='px-4 py-1 border rounded-3xl'>+ Text</button>
+      </div>
     </div>
   );
-}
+});
 
 export default CodeCell;
+
+type CodeCellProps = {
+  content: string;
+  onChange: (v: string | undefined) => void;
+};
